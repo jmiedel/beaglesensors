fca7327d7b4e345bd75ca05a02fbd725fae82952
jmiedel <jmiedel@andrew.cmu.edu>
2014-06-19 14:45:51 +0000

    a comment change

--- server.py
+++ server.py
@@ -16,8 +16,6 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         s.end_headers()
         s.wfile.write("<html><head><title>Title goes here.</title></head>")
         s.wfile.write("<body><p>This is a test.</p>")
-        # If someone went to "http://something.somewhere.net/foo/bar/",
-        # then s.path equals "/foo/bar/".
         
         s.wfile.write("<p>You accessed path: %s</p>" % s.path)
         s.wfile.write("</body></html>")
